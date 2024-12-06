2258c0f7da137374f2f4e053f5361db97b0e15dc
aleng1106 <ltlobygg@163.com>
2024-12-06 10:05:05 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 
+
+const articleType = 1;
+function getArticleTem() {
+  if (articleType === 0) {
+    return <div>我是猪猪乐1</div>
+  } else if (articleType === 1) {
+    return <div>我是猪猪乐2</div>
+  } else {
+    return <div>我是猪猪乐3</div>
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App1">
+      {getArticleTem()}
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, // 匹配 .js 和 .jsx 文件
+                exclude: /node_modules/, // 排除 node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'], // React + ES6
+                    },
+                },
+            },
+        ],
+    },
+};
