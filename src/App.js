17f11c3731f96dcd1339f16f2396be477bb51762
aleng1106 <ltlobygg@163.com>
2024-12-06 21:20:32 +0800

    add comment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 /**
  * App.js
  */
-import './index.scss'
-import avatar from './images/bozai.png'
+import './index.scss';
+import avatar from './images/bozai.png';
 import { useState } from 'react';
+import _ from 'lodash';
+import classNames from 'classnames';
 
 // 评论列表数据
 const list = [
@@ -14,37 +16,38 @@ const list = [
     user: {
       uid: '13258165',
       avatar: '',
-      uname: '周杰伦',
+      uname: '周杰伦'
     },
     // 评论内容
     content: '哎哟，不错哦',
     // 评论时间
     ctime: '10-18 08:15',
-    like: 88,
+    like: 88
   },
   {
     rpid: 2,
     user: {
       uid: '36080105',
       avatar: '',
-      uname: '许嵩',
+      uname: '许嵩'
     },
     content: '我寻你千百度 日出到迟暮',
     ctime: '11-13 11:29',
-    like: 88,
+    like: 88
   },
   {
     rpid: 1,
     user: {
       uid: '30009257',
       avatar,
-      uname: '黑马前端',
+      uname: '黑马前端'
     },
     content: '学前端就来黑马',
     ctime: '10-19 09:00',
-    like: 66,
-  },
-]
+    like: 66
+  }
+];
+
 // 当前登录用户信息
 const user = {
   // 用户id
@@ -52,8 +55,8 @@ const user = {
   // 用户头像
   avatar,
   // 用户昵称
-  uname: '黑马前端',
-}
+  uname: '黑马前端'
+};
 
 /**
  * 导航 Tab 的渲染和操作
@@ -67,27 +70,47 @@ const user = {
 // 导航 Tab 数组
 const tabs = [
   { type: 'hot', text: '最热' },
-  { type: 'time', text: '最新' },
-]
-
+  { type: 'time', text: '最新' }
+];
 
 function Button() {
-  return <button className="btn">click me2</button>
+  return <button className="btn">click me2</button>;
 }
 
 const articleType = 1;
+
 function getArticleTem() {
   if (articleType === 0) {
-    return <div>我是猪猪乐1</div>
+    return <div>我是猪猪乐1</div>;
   } else if (articleType === 1) {
-    return <div>我是猪猪乐2</div>
+    return <div>我是猪猪乐2</div>;
   } else {
-    return <div>我是猪猪乐3</div>
+    return <div>我是猪猪乐3</div>;
   }
 }
 
 const App = () => {
-  const [commentList, setCommentList] = useState(list)
+  const [commentList, setCommentList] = useState(_.orderBy(list, 'like', 'desc'));
+  const [type, setType] = useState('hot');
+
+  const handleDel = (id) => {
+    console.log(id);
+    setCommentList(commentList.filter((item) => item.rpid !== id));
+  };
+
+  const handleTabChange = (tab) => {
+    console.log(tab);
+    setType(tab.type);
+    if (tab.type === 'hot') {
+      //根据点赞数量排序
+      setCommentList(_.orderBy(commentList, 'like', 'desc'));
+    } else {
+      console.log(_.orderBy(commentList, 'ctime', 'desc'));
+
+      setCommentList(_.orderBy(commentList, 'ctime', 'desc'));
+    }
+  };
+
   return (
     <div className="app">
       {/* 导航 Tab */}
@@ -100,8 +123,16 @@ const App = () => {
           </li>
           <li className="nav-sort">
             {/* 高亮类名： active */}
-            <span className='nav-item'>最新</span>
-            <span className='nav-item'>最热</span>
+            {tabs.map((tab) => (
+              // className={`nav-item ${type === tab.type && 'active'}`}
+              <span
+                key={tab.type}
+                className={classNames('nav-item', { active: type === tab.type })}
+                onClick={() => handleTabChange(tab)}
+              >
+                {tab.text}
+              </span>
+            ))}
           </li>
         </ul>
       </div>
@@ -117,10 +148,7 @@ const App = () => {
           </div>
           <div className="reply-box-wrap">
             {/* 评论框 */}
-            <textarea
-              className="reply-box-textarea"
-              placeholder="发一条友善的评论"
-            />
+            <textarea className="reply-box-textarea" placeholder="发一条友善的评论" />
             {/* 发布按钮 */}
             <div className="reply-box-send">
               <div className="send-text">发布</div>
@@ -130,44 +158,42 @@ const App = () => {
         {/* 评论列表 */}
         <div className="reply-list">
           {/* 评论项 */}
-          {commentList.map(item => (<div key={item.rpid} className="reply-item">
-            {/* 头像 */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt=""
-                  src={item.avatar}
-                />
+          {commentList.map((item) => (
+            <div key={item.rpid} className="reply-item">
+              {/* 头像 */}
+              <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                  <img className="bili-avatar-img" alt="" src={item.avatar} />
+                </div>
               </div>
-            </div>
 
-            <div className="content-wrap">
-              {/* 用户名 */}
-              <div className="user-info">
-                <div className="user-name">{item.user.uname}</div>
-              </div>
-              {/* 评论内容 */}
-              <div className="root-reply">
-                <span className="reply-content">{item.content}</span>
-                <div className="reply-info">
-                  {/* 评论时间 */}
-                  <span className="reply-time">{item.ctime}</span>
-                  {/* 评论数量 */}
-                  <span className="reply-time">点赞数:{item.like}</span>
-                  <span className="delete-btn">
-                    删除
-                  </span>
-
+              <div className="content-wrap">
+                {/* 用户名 */}
+                <div className="user-info">
+                  <div className="user-name">{item.user.uname}</div>
+                </div>
+                {/* 评论内容 */}
+                <div className="root-reply">
+                  <span className="reply-content">{item.content}</span>
+                  <div className="reply-info">
+                    {/* 评论时间 */}
+                    <span className="reply-time">{item.ctime}</span>
+                    {/* 评论数量 */}
+                    <span className="reply-time">点赞数:{item.like}</span>
+                    {user.uid === item.user.uid && (
+                      <span className="delete-btn" onClick={() => handleDel(item.rpid)}>
+                        删除
+                      </span>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>))}
-
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
